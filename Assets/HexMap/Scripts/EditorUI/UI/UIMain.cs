4013900b0b84d466bb16f54e0760a2b7511af71b
LiuYuFei <535136811@qq.com>
2021-01-13 15:56:18 +0800

    lxf

--- Assets/HexMap/Scripts/EditorUI/UI/UIMain.cs
+++ Assets/HexMap/Scripts/EditorUI/UI/UIMain.cs
@@ -29,14 +29,21 @@ namespace HexMap
         #endregion
 
         #region minimap
-
+        public Transform pointer;
         public Text textPosition;
 
         #endregion
-
+        MapCamera mapCamera;
+        HexGrid hexGrid;
         void Awake()
         {
             drapDpwnEditorModel.onValueChanged.AddListener(OnValueChanged_EditorModel);
+            btnGoto.onClick.AddListener(OnGotoButtonClick);
+        }
+        private void Start()
+        {
+            mapCamera = Runtime.HexMap.instance.mapCamera;
+            hexGrid = Runtime.HexMap.instance.hexGrid;
         }
 
         public void Initialize(EditorModel model)
@@ -47,6 +54,33 @@ namespace HexMap
         void OnValueChanged_EditorModel(int value)
         {
             Runtime.HexMap.instance.SetEditorModel((EditorModel)value);
+        }
+        void OnGotoButtonClick() 
+        {
+            int cellX;
+            int cellY;
+            if (int.TryParse(inputX.text,out cellX) && int.TryParse(inputY.text, out cellY))
+            {
+                Runtime.HexMap.instance.mapCamera.MoveToCell(cellX, cellY);
+                var currPos = Runtime.HexMap.instance.mapCamera.GetCenterPosition();
+                var chunkXZ = Runtime.HexMap.instance.hexGrid.PositionToCellXZ(currPos);
+                Debug.Log(chunkXZ);
+            }
+        }
+        private void Update()
+        {
+            RefreshMinmpaShow();
+        }
+
+        private void RefreshMinmpaShow() 
+        {
+            var currPos = mapCamera.GetCenterPosition();
+            var chunkXZ = hexGrid.PositionToCellXZ(currPos);
+            RectTransform rect = pointer.parent.GetComponent<RectTransform>();
+            float posX = chunkXZ.x * rect.rect.width / hexGrid.gridRowCount - rect.rect.width / 2;
+            float posY = chunkXZ.y * rect.rect.height / hexGrid.gridColumnCount - rect.rect.height / 2;
+            pointer.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(posX, -posY, 0);
+            textPosition.text = string.Format("X:{0} Y:{1}", chunkXZ.x, chunkXZ.y);
         }
     }
 }
