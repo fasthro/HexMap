c4f1b21cc3f084624f33394f4586def3944041d0
LiuYuFei <535136811@qq.com>
2021-01-14 11:19:20 +0800

    lxf

--- Assets/HexMap/Scripts/EditorUI/UI/UIMain.cs
+++ Assets/HexMap/Scripts/EditorUI/UI/UIMain.cs
@@ -1,8 +1,9 @@
 ﻿/*
  * @Author: fasthro
  * @Date: 2021-01-13 11:43:00
- * @Description: 
+ * @Description:
  */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,7 @@ namespace HexMap
 {
     public class UIMain : MonoBehaviour
     {
-        #region  top
+        #region top
 
         public Dropdown drapDpwnEditorModel;
 
@@ -23,23 +24,25 @@ namespace HexMap
 
         public Button btnSave;
 
-        #endregion
-
-        #region bottom
-        #endregion
+        #endregion top
 
         #region minimap
+
+        public RectTransform planeRect;
         public Transform pointer;
         public Text textPosition;
+        public Camera UICamera;
+        #endregion minimap
 
-        #endregion
-        MapCamera mapCamera;
-        HexGrid hexGrid;
-        void Awake()
+        private MapCamera mapCamera;
+        private HexGrid hexGrid;
+
+        private void Awake()
         {
             drapDpwnEditorModel.onValueChanged.AddListener(OnValueChanged_EditorModel);
             btnGoto.onClick.AddListener(OnGotoButtonClick);
         }
+
         private void Start()
         {
             mapCamera = Runtime.HexMap.instance.mapCamera;
@@ -51,15 +54,16 @@ namespace HexMap
             drapDpwnEditorModel.value = (int)model;
         }
 
-        void OnValueChanged_EditorModel(int value)
+        private void OnValueChanged_EditorModel(int value)
         {
             Runtime.HexMap.instance.SetEditorModel((EditorModel)value);
         }
-        void OnGotoButtonClick() 
+
+        private void OnGotoButtonClick()
         {
             int cellX;
             int cellY;
-            if (int.TryParse(inputX.text,out cellX) && int.TryParse(inputY.text, out cellY))
+            if (int.TryParse(inputX.text, out cellX) && int.TryParse(inputY.text, out cellY))
             {
                 Runtime.HexMap.instance.mapCamera.MoveToCell(cellX, cellY);
                 var currPos = Runtime.HexMap.instance.mapCamera.GetCenterPosition();
@@ -67,12 +71,23 @@ namespace HexMap
                 Debug.Log(chunkXZ);
             }
         }
+
         private void Update()
         {
             RefreshMinmpaShow();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 pos;
+                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(planeRect, Input.mousePosition, null, out pos))
+                {
+                    Debug.Log("mousePosition :" + pos);
+                    UIPositionToCellXZAndGoto(pos);
+                }
+            }
         }
 
-        private void RefreshMinmpaShow() 
+        private void RefreshMinmpaShow()
         {
             var currPos = mapCamera.GetCenterPosition();
             var chunkXZ = hexGrid.PositionToCellXZ(currPos);
@@ -81,6 +96,20 @@ namespace HexMap
             float posY = chunkXZ.y * rect.rect.height / hexGrid.gridColumnCount - rect.rect.height / 2;
             pointer.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(posX, -posY, 0);
             textPosition.text = string.Format("X:{0} Y:{1}", chunkXZ.x, chunkXZ.y);
+        }
+
+        private void UIPositionToCellXZAndGoto(Vector2 pos) 
+        {
+            if (pos.x < planeRect.rect.width / 2 && pos.x > -planeRect.rect.width / 2)
+            {
+                if (pos.y < planeRect.rect.height / 2 && pos.y > -planeRect.rect.height / 2)
+                {
+                    int cellX = (int)((pos.x + planeRect.rect.width / 2) / planeRect.rect.width * hexGrid.gridRowCount);
+                    int cellY = (int)(-(pos.y - planeRect.rect.height / 2) / planeRect.rect.height * hexGrid.gridColumnCount);
+
+                    Runtime.HexMap.instance.mapCamera.MoveToCell(cellX, cellY);
+                }
+            }
         }
     }
 }
