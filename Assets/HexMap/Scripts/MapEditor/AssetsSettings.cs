102ec4301613e301e1f1109357d1d3e691c655a5
LiuYuFei <535136811@qq.com>
2021-01-18 14:25:22 +0800

    AssetsSettings

--- Assets/HexMap/Scripts/MapEditor/AssetsSettings.cs
+++ Assets/HexMap/Scripts/MapEditor/AssetsSettings.cs
@@ -2,10 +2,13 @@
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+
 #if UNITY_EDITOR
+
 using UnityEditor;
 
 #endif
+
 namespace HexMap
 {
     [System.Serializable]
@@ -34,6 +37,19 @@ namespace HexMap
         public List<Dropdown.OptionData> GetDropdownAssetTypes()
         {
             return assetsTypes.Select(assetType => new Dropdown.OptionData(assetType.typeName)).ToList();
+        }
+
+        public List<Asset> GetAssetListByTypeName(string typeName)
+        {
+            List<Asset> assets = new List<Asset>();
+            for (int i = 0; i < assetsTypes.Length; i++)
+            {
+                if (assetsTypes[i].typeName == typeName)
+                {
+                    assets = new List<Asset>(assetsTypes[i].assets);
+                }
+            }
+            return assets;
         }
     }
 }
